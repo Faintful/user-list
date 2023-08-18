81116a0fdf3224e466f8106a95aad6b0777dea85
faint <faintcsr@gmail.com>
2023-08-18 15:25:27 +0200

    Added state and change handler to Form component. Changed logic in Input component and passed down handler

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import './Form.css';
 import Input from './Input';
 
+const initialState = {
+  name: '',
+  age: 0,
+};
+
 export default function Form() {
+  const [inputState, setInput] = useState(initialState);
+
+  const onChangeHandler = ({ target: { id, value } }) => {
+    setInput((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
   return (
     <form className=''>
       {['Name', 'Age'].map((element, index) => (
-        <Input tag={element} key={index} />
+        <Input tag={element} key={index} onChange={onChangeHandler} />
       ))}
     </form>
   );
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,12 @@
 import './Input.css';
 
-export default function Input({ tag }) {
+export default function Input({ tag, onChange }) {
   return (
-    <div className=''>
-      <label htmlFor={tag}>
-        <input id={tag}>{tag}</input>
+    <p className=''>
+      <label htmlFor={tag.toLowerCase()}>
+        {tag}
+        <input onChange={onChange} id={tag.toLowerCase()} />
       </label>
-    </div>
+    </p>
   );
 }
